e05622b799311a499c3ac90cf6ea84c71357eee5
Aleksei Krasnov <no@no.ru>
2024-02-17 15:01:45 +0300

    task complete

--- nim_engine.py
+++ nim_engine.py
@@ -1,17 +1,26 @@
 from random import randint
-
-_holder = []
+MAX_BUNCHES = 5
+MAX_BUNCHES_SIZE = 20
+_holder = {}
+def settings():
+    pass
 def put_stones():
     global _holder
-    _holder = []
-    for i in range(5):
-        _holder.append(randint(1,20))
+    _holder = {}
+    for i in range(1,MAX_BUNCHES+1):
+        _holder[i] = randint(1,MAX_BUNCHES_SIZE)
     pass
 def take_from_bunch(position, quantity):
-    if 1 <= position < len(_holder):
+    if position in _holder:
         _holder[position] -= quantity
+        return True
+    else:
+        return False
 def get_bunches():
-    return _holder
+    res = []
+    for key in sorted(_holder.keys()):
+        res.append(_holder[key])
+    return res
     pass
 def is_game_over():
     return sum(_holder) == 0
--- practice_game_nim.py
+++ practice_game_nim.py
@@ -8,11 +8,13 @@ while True:
     print("Текущая позиция")
     print(get_bunches())
     user_color = 'blue' if user_number == 1 else 'yellow'
-    cprint("Ход игрока {}".format(user_number),color=user_color)0
+    cprint("Ход игрока {}".format(user_number),color=user_color)
     pos = input("Откуда берем?")
     qua = input("Сколько берем?")
-    take_from_bunch(quantity=int(qua), position=int(pos))
-    if is_game_over():
-        break
+    step_sucess = take_from_bunch(quantity=int(qua), position=int(pos))
+    if step_sucess:
+        user_number = 2 if user_number == 1 else user_number = 1
+    else:
+        cprint("Невозможный ход",color='red')
     user_number = 2 if user_number == 1 else 1
 print("Выиграл игрок № ",user_number)
